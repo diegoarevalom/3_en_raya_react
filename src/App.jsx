14eef3173114bbee3bf8045e7f949cda4185ee37
Diego Arevalo <diego.arevalo@inacapmail.cl>
2024-02-07 13:08:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import { useState, useEffect } from "react"
+import confetti from "canvas-confetti"
+
+import { Square } from "./components/Square"
+import { TURNS} from "../constants"
+import { checkWinnerFrom } from "./board"
+import { WinnerModal } from "./components/WinnerModal"
+
+
+function App() {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if(boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+  
+  //null es que no hay ganador, false es que hay un empate
+  const [winner, setWinner] = useState(null)
+
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+
+  }
+
+  const checkEndGame = (newBoard) => {
+    // revisamos si hay un empate
+    // si no hay mas espacios vacios
+    // en el tablero
+    return newBoard.every((square) => square !== null)
+  }
+
+
+  const updateBoard = (index) => {
+    //No actualizamos esta posicion
+    //Si ya tiene algo
+    if(board[index] || winner) return
+    //Actualizar el turno
+    const newBoard = [...board]
+    newBoard[index] = turn // 
+    setBoard(newBoard)
+    //Cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //Guardar aqui partida
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
+
+    //revisamos si hay ganador 
+    const newWinner = checkWinnerFrom(newBoard)
+    if(newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)){
+      setWinner(false) // Empate
+    }
+  }
+
+  return (
+  <main className="board">
+    <h1>Gato</h1>
+    <button onClick={resetGame}>Reset del juego</button>
+    <section className="game">
+      {
+        board.map((square, index) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+            >
+              {square}
+            </Square>
+          )
+        })
+      }
+    </section>
+
+    
+    <section className="turn">
+      <Square isSelected={turn === TURNS.X}>
+        {TURNS.X}
+      </Square>
+      <Square isSelected={turn === TURNS.O}>
+        {TURNS.O}
+      </Square>
+
+    </section>
+
+    <WinnerModal resetGame={resetGame} winner={winner}/>
+  </main>
+  )
+}
+
+export default App
---
+++ src/board.js
@@ -0,0 +1,19 @@
+import { WINNER_COMBOS } from "../constants"
+
+export const checkWinnerFrom = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    //para ver si X u O gano
+    for(const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && // 0 y 3 -> x -> x u o -> o
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+
+    //Si no hay ganador
+    return null
+  }
