a371c3d4b79847c6bb9375fb31bef1452b14d25a
Diego <diego.arevalo@inacapmail.cl>
2025-05-03 22:13:29 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react"
 import confetti from "canvas-confetti"
-
 import { Square } from "./components/Square"
 import { TURNS} from "../constants"
 import { checkWinnerFrom } from "./board"
